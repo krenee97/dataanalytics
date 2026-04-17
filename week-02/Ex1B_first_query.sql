5f680f46cac37f4d48096dfef393a6bf61d7844d
kendra tyler <ktyler@my.yearupunited.org>
2026-04-16 22:14:18 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about any and everything for use by everyone.
 */
+
+USE sakila;
+Select * FROM actor; -- Retrieved 200 records
+SELECT * FROM film; -- Retrieved 1000 records
