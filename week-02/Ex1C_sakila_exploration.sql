a7edab24cb7fbb45eca0514f419dd66e695cd460
kendra tyler <ktyler@my.yearupunited.org>
2026-04-16 22:51:08 -0400

    Exercise 1.C complete

---
+++ week-02/Ex1C_sakila_exploration.sql
@@ -0,0 +1,12 @@
+/*
+a) The information that you will find is the actor_id, firstname, lastname and last update about each individual.alter
+b) The information in this table incucles the film id, title, description, release year, language id, original lanuage id, rental duration, rental rate, length, replacement cost, rating, special features and last update. All of the information they will need to know for the make of the movie or film
+c) You will find actor id and film id inside the film actor column.alter
+d) Its shows nummbers but its not money signs or saying exactly whst the information is for. I get the numbers but are they for the second, hour or etc. So I will say its hard to read. 
+e) Its kind of the same in the inventory table. It listed number some showing duplicates. but not showing anymore informaiton. Not showing what the inventory is for.alter
+f) The name of the actors and film names. They all related to each other becasue they all sum up to the samething just put togwther in one spot.alter
+*/
+
+SELECT actor_id FROM actor;
+SELECT film_id FROM film;
+
