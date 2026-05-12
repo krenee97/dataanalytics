69b87d6d0cb819bd3d63c211ac5ee0568c773c99
kendra tyler <tylerkendra01@gmail.com>
2026-05-11 23:38:33 -0400

    complete Ex1A Hello

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,18 @@
 print('Hello World!')
+
+message = 'Hello World!'
+print(message)
+
+# "Hello World!" prints twice becasue ...
+
+# Displaying dollars and cents 
+dollars = 3
+cents = .50
+print(dollars + cents)
+# The result is 3.5 - Python added the numbers together mathmatically 
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + "" + c_str)
