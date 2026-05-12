78b5e2350269f3c639d3c01ccf4fd9520be3296a
kendra tyler <tylerkendra01@gmail.com>
2026-05-12 00:22:22 -0400

    complete Ex1B

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,17 @@
+customer_id = 0 # I used a number because IDs are usually numeric
+customer_name = '' # I used a string but maybe first and last should be seperate variables
+customer_gender = '' #Im Assuming M/F/Other - may need more options
+customer_dob = '' # I used a string but may need a speical date Format MM/DD/YYYY 
+drivers_license_number = '' # I used a string because it may contain letters and numbers 
+auto_policy_number = 0 # I used a number but it might need to be a srting if it has dashes or letters 
+
+my_name = 'Kendra Tyler'
+city_state = 'Glenarden,Maryland'
+
+
+# A) Resvered words are False, def, if , raise, None, del, import, return,True, elif, in, try, and, else, is,while, as, except lambda, with, assert, finally, nonlocal yield, break, for, not, class, form, or, continue, gobal, pass
+# B) if - used to make a decision in code, like "if this is true, do this"
+# for - used to loop/repeat through a list of items
+# return - used to send a value back from a function
+# True - ^^ a boolean value meaning yes/correct
+# while - used to repeat code as long as a condition is true
