95670c07d87d2509dd32970f59fa3b95ff41ad46
kendra tyler <tylerkendra01@gmail.com>
2026-05-13 20:10:24 -0400

    complete Ex2A tips

---
+++ week-05/sales_stats.py
@@ -0,0 +1,19 @@
+import statistics 
+
+def analyze_sales(analyst, region, sales):
+ mean = statistics.mean(sales)
+ median = statistics.median(sales)
+ mode = statistics.mode(sales)
+ stdev = statistics.stdev(sales)
+ total = sum(sales)
+ high = max(sales)
+ low = min(sales)
+ return mean, median, mode, stdev, high, low
+
+analyst = input("analyst name:Kendra")
+region = input("region: East")
+
+print("Enter daily sales for 7 days:")
+sales = [float(input(f"day{1+1}:$")) for i in range(7)]
+
+mean, median, mode
