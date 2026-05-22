52db5a58e62e823aefcf181119e7ee9323c1dcf3
kendra tyler <tylerkendra01@gmail.com>
2026-05-21 20:31:02 -0400

    complete Ex4A_ReadWriteFiles/open_and_write.py

--- week-06/Ex4A_ReadWriteFiles/open_and_write.py
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -1,4 +1,5 @@
 # Practcing read/write - open and write to a file 
 
 f = open("about_me.txt", "a")
+f.write("\nIf you could do anything for your perfect night out, where would you go and what would you do?\n")
 f.close()
